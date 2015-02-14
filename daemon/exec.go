92436f9bc36b4c1aabfcbafa9a53d24f7ffd3a7e
John Howard (VM) <John.Howard@microsoft.com>
2015-02-13 17:51:18 -0800

    More lxc factoring out for Windows in daemon

--- daemon/exec.go
+++ daemon/exec.go
@@ -111,55 +111,6 @@ func (d *Daemon) getActiveContainer(name string) (*Container, error) {
 	return container, nil
 }
 
-func (d *Daemon) ContainerExecCreate(job *engine.Job) engine.Status {
-	if len(job.Args) != 1 {
-		return job.Errorf("Usage: %s [options] container command [args]", job.Name)
-	}
-
-	if strings.HasPrefix(d.execDriver.Name(), lxc.DriverName) {
-		return job.Error(lxc.ErrExec)
-	}
-
-	var name = job.Args[0]
-
-	container, err := d.getActiveContainer(name)
-	if err != nil {
-		return job.Error(err)
-	}
-
-	config, err := runconfig.ExecConfigFromJob(job)
-	if err != nil {
-		return job.Error(err)
-	}
-
-	entrypoint, args := d.getEntrypointAndArgs(nil, config.Cmd)
-
-	processConfig := execdriver.ProcessConfig{
-		Tty:        config.Tty,
-		Entrypoint: entrypoint,
-		Arguments:  args,
-	}
-
-	execConfig := &execConfig{
-		ID:            utils.GenerateRandomID(),
-		OpenStdin:     config.AttachStdin,
-		OpenStdout:    config.AttachStdout,
-		OpenStderr:    config.AttachStderr,
-		StreamConfig:  StreamConfig{},
-		ProcessConfig: processConfig,
-		Container:     container,
-		Running:       false,
-	}
-
-	container.LogEvent("exec_create: " + execConfig.ProcessConfig.Entrypoint + " " + strings.Join(execConfig.ProcessConfig.Arguments, " "))
-
-	d.registerExecCommand(execConfig)
-
-	job.Printf("%s\n", execConfig.ID)
-
-	return engine.StatusOK
-}
-
 func (d *Daemon) ContainerExecStart(job *engine.Job) engine.Status {
 	if len(job.Args) != 1 {
 		return job.Errorf("Usage: %s [options] exec", job.Name)
